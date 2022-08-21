59c1b02f489e9a2735a48c46b4c5f76371612ce0
Katerina <katerina4484@mail.ru>
2022-08-21 15:50:51 +0500

    ex27

---
+++ ex27/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+int sum = 0;
+int a = Mod(number);
+
+while(a!=0)
+{
+    sum = sum + a;
+}
+Console.WriteLine(sum);
+
+
+int Mod(int num)
+{
+    return number%10;
+}
