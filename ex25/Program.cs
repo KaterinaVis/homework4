da45ae4966fabfa29c3f306b5da69ad4084874b7
Katerina <katerina4484@mail.ru>
2022-08-21 16:53:26 +0500

    ex25

---
+++ ex25/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+int a = ReadInt("Введите число А: ");
+int b = ReadInt("Введите число В: ");
+
+int multip = a;
+
+for(int count = 2; count <= b; count ++ )
+{
+    multip=multip*a;
+}
+
+Console.Write(multip);
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
